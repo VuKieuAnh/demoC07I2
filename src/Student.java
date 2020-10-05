7434e987cdabdf28137acce03f01e7eb92143a82
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-10-05 14:04:45 +0700

    init

---
+++ src/Student.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Student {
+    private int age;
+    public static void main(String[] args) {
+        final float PI = 3.14f;
+//        PI = 3.2f;
+//        System.out.printf("Mời bạn nhập vào số tháng");
+//        int mounth;
+//        Scanner scanner = new Scanner(System.in);
+//        mounth =  scanner.nextInt();
+//        int daysOfMounth = 0;
+//        switch (mounth) {
+//            case 1:
+//            case 3:
+//            case 5:
+//            case 7:
+//            case 8:
+//            case 10:
+//            case 12:
+//                daysOfMounth = 31;
+//                System.out.printf("tháng " + mounth  + " có " + daysOfMounth + " ngày");
+//                break;
+//            case 2:
+//            case 4:
+//            case 6:
+//            case 9:
+//            case 11:
+//                daysOfMounth = 30;
+//                System.out.printf("tháng " + mounth  + " có " + daysOfMounth + " ngày");
+//                break;
+//            default:
+//                System.out.printf("Đây không phải tháng");
+//
+//        }
+    }
+}
